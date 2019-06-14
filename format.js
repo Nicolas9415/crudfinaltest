b74183a738184d1e9cf9e6f7530718377ced836b
NicolasSimmonds <nicolas.simmonds@endava.com>
2019-06-14 16:59:59 -0500

    fixed dates

--- format.js
+++ format.js
@@ -1,14 +1,14 @@
-const fs=require('fs')
+const fs = require("fs");
 
-let rawdata = fs.readFileSync('db.json');  
-let student = JSON.parse(rawdata);  
-console.log(student);  
+let rawdata = fs.readFileSync("test.json");
+let student = JSON.parse(rawdata);
+console.log(student);
 
-for(let i=0;i<student.users.length;i++)
-{
-student.users[i].appointment.start=new Date(student.users[i].appointment.start).getTime()
-student.users[i].appointment.end=new Date(student.users[i].appointment.end).getTime()
+for (let i = 0; i < student.length; i++) {
+  student[i].appointment[0].start = new Date(student[i].appointment[0].start).getTime();
+  student[i].appointment[0].end = new Date(student[i].appointment[0].end).getTime();
 
+  student[i].appointment = student[i].appointment[0];
 }
 
-fs.writeFile('newFile.json',JSON.stringify(student.users),()=>null)
+fs.writeFile("newFile.json", JSON.stringify(student), () => null);
