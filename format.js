6b42e20c485046117fdd67ab6b9807d171a6d48f
Nicolas <nicolas.simmonds@endava.com>
2019-06-17 22:19:03 -0500

    changed json location to object

--- format.js
+++ format.js
@@ -1,14 +1,10 @@
 const fs = require("fs");
 
-let rawdata = fs.readFileSync("test.json");
+let rawdata = fs.readFileSync("db.json");
 let student = JSON.parse(rawdata);
-console.log(student);
 
-for (let i = 0; i < student.length; i++) {
-  student[i].appointment[0].start = new Date(student[i].appointment[0].start).getTime();
-  student[i].appointment[0].end = new Date(student[i].appointment[0].end).getTime();
-
-  student[i].appointment = student[i].appointment[0];
+for (let i = 0; i < student.users.length; i++) {
+  student.users[i].location = student.users[i].location[0];
 }
 
 fs.writeFile("newFile.json", JSON.stringify(student), () => null);
